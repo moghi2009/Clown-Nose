7334d1d475a916f22cf19ae758406f818667e705
moghi2009 <84376170+moghi2009@users.noreply.github.com>
2022-05-26 20:57:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = 0;
+noseY = 0;
+function preload(){
+    clownNose = loadImage('https://i.postimg.cc/bN0WzSc5/580b57fbd9996e24bc43bed5.png');
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(clownNose, noseX, noseY, 30, 30);
+}
+
+function takeSnapshot(){
+    save('filteredImage.png');
+}
+
+function modelLoaded(){
+    console.log("The model is Loaded");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose X = " +results[0].pose.nose.x);
+        console.log("nose Y = " +results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 15;
+        noseY = results[0].pose.nose.y - 15;
+    }
+}
+
+
